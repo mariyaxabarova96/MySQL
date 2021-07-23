9a01e32d4ced8a8a475fdd03e5f429d5f33931e6
mariyaxabarova96 <mariya.xabarova.96@mail.ru>
2021-07-23 14:27:09 +0300

    11

---
+++ 11/Homework_11.sql
@@ -0,0 +1,119 @@
+
+-- Оптимизация запросов
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+	created_at DATETIME NOT NULL,
+	table_name VARCHAR(45) NOT NULL,
+	str_id BIGINT(20) NOT NULL,
+	name_value VARCHAR(45) NOT NULL
+) ENGINE = ARCHIVE;
+
+
+-- **********  TRIGGER ON users  **********
+DROP TRIGGER IF EXISTS watchlog_users;
+delimiter //
+CREATE TRIGGER watchlog_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'users', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+-- **********  TRIGGER ON catalogs  **********
+DROP TRIGGER IF EXISTS watchlog_catalogs;
+delimiter //
+CREATE TRIGGER watchlog_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+-- **********  TRIGGER ON products  **********
+delimiter //
+CREATE TRIGGER watchlog_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'products', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+-- **********  Tests for users  **********
+SELECT * FROM users;
+SELECT * FROM logs;
+
+INSERT INTO users (name, birthday_at)
+VALUES ('Кнехт', '1900-01-01');
+
+SELECT * FROM users;
+SELECT * FROM logs;
+
+INSERT INTO users (name, birthday_at)
+VALUES ('Liu Kangh', '1900-01-01'),
+		('Sub-Zero', '1103-01-01'),
+		('Scorpion', '1103-01-01'),
+		('Raiden', '0000-00-01');
+
+SELECT * FROM users;
+SELECT * FROM logs;
+
+
+-- **********  Tests for catalogs  **********
+SELECT * FROM catalogs;
+SELECT * FROM logs;
+
+INSERT INTO catalogs (name)
+VALUES ('Оперативная память'),
+		('Куллера'),
+		('Аксессуары');
+
+SELECT * FROM catalogs;
+SELECT * FROM logs;
+
+
+-- **********  Tests for products  **********
+SELECT * FROM products;
+SELECT * FROM logs;
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ('PATRIOT PSD34G13332', 'Оперативная память', 3000.00, 13),
+		('DARK ROCK PRO 4 (BK022)', 'Куллера', 500.00, 14),
+		('Коврик', 'Коврик для мыши', 150.00, 15);
+
+SELECT * FROM products;
+SELECT * FROM logs;
+
+-- NoSQL
+
+-- 1. В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+
+-- Хэш таблицы (ключ - ip адрес, а в значении число посещений)
+
+-- 2. 
+hset user_email name email
+hset email_user email name
+
+hget user_email "name" -- поиск почты
+hget email_user "email" -- поиск имени
+
+-- 3. 
+
+shop.catalogs.insertMany( [
+      { _id: 1, name: "Процессоры"},
+      { _id: 2, name: "Материнские платы"},
+      { _id: 3 ,name: "Видеокарты"}
+   ] );
+
+shop.products.insert({
+    name: "Intel Core i3-8100",
+    description: "Процессор для настольных персональных компьютеров, основанных на платформе Intel.",
+    price: 7890.00,
+    catalog: 1
+})
